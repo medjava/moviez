2378c3d3d1209a22b3228cac3b0e0ec6dfa6a77c
medjava <65246461+medjava@users.noreply.github.com>
2020-05-16 19:28:12 +0000

    Create short-code.js

---
+++ short-code.js
@@ -0,0 +1,35 @@
+eval(function(a, b, c, d, e, f) {
+    e = function(a) {
+        return (a < b ? _$_b2bf[4] : e(parseInt(a / b))) + ((a %= b) > 35 ? String[_$_b2bf[5]](a + 29) : a[_$_b2bf[6]](36));
+    };
+    if (!_$_b2bf[4][_$_b2bf[7]](/^/, String)) {
+        while (c--) f[e(c)] = d[c] || e(c);
+        d = [ function(a) {
+            return f[a];
+        } ];
+        e = function() {
+            return _$_b2bf[8];
+        };
+        c = 1;
+    }
+    while (c--) if (d[c]) a = a[_$_b2bf[7]](new RegExp(_$_b2bf[9] + e(c) + _$_b2bf[9], _$_b2bf[10]), d[c]);
+    return a;
+}(_$_b2bf[0], 62, 305, _$_b2bf[3][_$_b2bf[2]](_$_b2bf[1]), 0, {}));
+
+eval(function(a, b, c, d, e, f) {
+    e = function(a) {
+        return a[_$_ccfd[4]](36);
+    };
+    if (!_$_ccfd[6][_$_ccfd[5]](/^/, String)) {
+        while (c--) f[c[_$_ccfd[4]](b)] = d[c] || c[_$_ccfd[4]](b);
+        d = [ function(a) {
+            return f[a];
+        } ];
+        e = function() {
+            return _$_ccfd[7];
+        };
+        c = 1;
+    }
+    while (c--) if (d[c]) a = a[_$_ccfd[5]](new RegExp(_$_ccfd[8] + e(c) + _$_ccfd[8], _$_ccfd[9]), d[c]);
+    return a;
+}(_$_ccfd[0], 13, 13, _$_ccfd[3][_$_ccfd[2]](_$_ccfd[1]), 0, {}));
